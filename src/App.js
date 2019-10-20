726b82f9eaf947c8adfb3b2cd390ed6bc119a72e
EC2 Default User <ec2-user@ip-172-31-43-70.us-east-2.compute.internal>
2019-10-20 19:50:33 +0000

    first commit client

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          VAI TOMAR NO CU FINALMENTE AAAAAaaaAAA.
+        </p>
+        <p>
+          agora vai.
         </p>
         <a
           className="App-link"
