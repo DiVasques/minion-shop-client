63fdb5ab3165e64453ecfe90a8954388e2d63957
EC2 Default User <ec2-user@ip-172-31-43-70.us-east-2.compute.internal>
2019-10-22 02:33:36 +0000

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://3zhx47dag1.execute-api.us-east-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_zwtNzkrZa",
+    APP_CLIENT_ID: "391h8hk1edicq1j7lfhnk3mae4",
+    IDENTITY_POOL_ID: "us-east-2:70521934-b96b-4fc5-ba32-8e72fe51bdc7"
+  }
+};
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>MinionShop</h1>
+        <p>The best place to purchase Minions miniatures (Miniontures?)</p>
+      </div>
+    </div>
+  );
+}
